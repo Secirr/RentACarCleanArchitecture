2b34552aaa2aff12a17815c28b15fbaa6b81b877
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-12-14 03:56:25 +0300

    Upload entities

---
+++ src/rentACarCleanArchitecture/Domain/Entities/Feature.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Domain.Entities
+{
+	public class Feature
+	{
+		public Feature()
+		{
+		}
+	}
+}
+
---
+++ src/rentACarCleanArchitecture/Domain/Entities/UserType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+	public class UserType : Entity<Guid>
+	{
+		public string Name { get; set; }
+
+
+		//Customer
+		//Seller
+		//Other
+	}
+}
+
