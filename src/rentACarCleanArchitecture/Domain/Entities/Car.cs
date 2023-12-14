cfd0ff426a23fc6221c3b4a0115d80739d0f79e9
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-12-14 04:00:50 +0300

    Adding ImageUrl for car Entity

--- src/rentACarCleanArchitecture/Domain/Entities/Car.cs
+++ src/rentACarCleanArchitecture/Domain/Entities/Car.cs
@@ -1,11 +1,71 @@
 ﻿using System;
+using Core.Persistence.Repositories;
+
 namespace Domain.Entities
 {
-	public class Car
+	public class Car : Entity<Guid>
 	{
-		public Car()
-		{
-		}
+		public string Name { get; set; }
+
+		public Guid ColorId { get; set; }
+
+		public Guid BrandId { get; set; }
+
+		public Guid ModelId { get; set; }
+
+		public Guid DriveTypeID { get; set; } // 4x4
+
+		public Guid TransmissionId { get; set; } // vites
+
+		public Guid FuelTypeId { get; set; }
+
+		public Guid StatuId { get; set; }
+
+		public Guid FeatureId { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public decimal DailyPrice { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public string Description { get; set; }
+
+        public int Year { get; set; }
+
+		public long Millage { get; set; } // km
+
+		public Double EngineSize { get; set; }
+
+		public int Door { get; set; }
+
+		public int Cylinder { get; set; }
+
+
+
+
+
+
+
+        public Feature Feature { get; set; }
+
+        public Color Color { get; set; }
+
+		public Statu Statu { get; set; }
+
+		public FuelType FuelType { get; set; }
+
+        public Transmission Transmission { get; set; }
+
+		public DriveType DriveType { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public Model Model { get; set; }
+
+
+
+		
 	}
 }
 
